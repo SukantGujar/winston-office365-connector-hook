2c43b0ab49f44e6a1a10c3f15a2563b4a2b5f235
Sukant Gujar <sukantgujar@yahoo.com>
2017-05-09 14:03:04 +0530

    Add message queue, no more lost messages :smiley:!

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var util = require('util'),
+    queue = require('./queue'),
     request = require('request'),
     winston = require('winston'),
     webColors = {
@@ -12,31 +13,60 @@ var util = require('util'),
         "white": "fff",
         "gray": "808080",
         "grey": "808080"
-    }
+    },
 
-Office365ConnectorHook = winston.transports.Office365ConnectorHook = function (options) {
-    this.name = options.name || 'office365connectorhook';
-    this.level = options.level || 'silly';
+    Office365ConnectorHook = winston.transports.Office365ConnectorHook = function (options) {
+        this.name = options.name || 'office365connectorhook';
+        this.level = options.level || 'silly';
 
-    this.hookUrl = options.hookUrl || null;
+        this.hookUrl = options.hookUrl || null;
 
-    this.webColors = options.colors || {};
+        this.webColors = options.colors || {};
 
-    this.prependLevel = options.prependLevel === undefined ? true : options.prependLevel;
-    this.appendMeta = options.appendMeta === undefined ? true : options.appendMeta;
+        this.prependLevel = options.prependLevel === undefined ? true : options.prependLevel;
+        this.appendMeta = options.appendMeta === undefined ? true : options.appendMeta;
 
-    this.formatter = options.formatter || null;
-};
+        this.formatter = options.formatter || null;
+    },
+    q = queue({ concurrency: 1, autostart: true });
 
 util.inherits(Office365ConnectorHook, winston.Transport);
 
-Office365ConnectorHook.prototype.log = function (level, msg, meta, callback) {
+ensureCallback = function (cb1, cb2) {
+    return function () {
+        try {
+            cb1.apply(null, arguments);
+        }
+        finally {
+            cb2();
+        }
+    }
+}
 
+deferredExecute = function (url, payload, callback) {
+    return function (cb) {
+        var safeCallback = ensureCallback(callback, cb);
+        request
+            .post(url)
+            .json(payload)
+            .on('response', function (response) {
+                if (response.statusCode === 200) {
+                    safeCallback(null, true);
+                    return;
+                }
+
+                safeCallback('Server responded with ' + response.statusCode);
+            })
+            .on('error', function (error) {
+                safeCallback(error);
+            });
+    }
+}
+
+Office365ConnectorHook.prototype.log = function (level, msg, meta, callback) {
     var color = winston.config.allColors[level],
         themeColor = this.webColors[level] || webColors[color],
         title = "";
-
-    // console.debug(themeColor);
 
     var message = '';
 
@@ -70,20 +100,7 @@ Office365ConnectorHook.prototype.log = function (level, msg, meta, callback) {
         themeColor: themeColor
     };
 
-    request
-        .post(this.hookUrl)
-        .json(payload)
-        .on('response', function (response) {
-            if (response.statusCode === 200) {
-                callback(null, true);
-                return;
-            }
-
-            callback('Server responded with ' + response.statusCode);
-        })
-        .on('error', function (error) {
-            callback(error);
-        });
+    q.push(deferredExecute(this.hookUrl, payload, callback));
 };
 
 module.exports = Office365ConnectorHook;
